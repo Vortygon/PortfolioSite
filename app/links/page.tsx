78c31f5e3905eac82679f99c589420d85dac54e3
Vortygon <50510432+Vortygon@users.noreply.github.com>
2025-08-04 05:02:36 +0700

    Update page.tsx

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   description: "Ссылочки",
 };
 
-function Link(props) {
+function Link(props: { src: string; icon: string | undefined; iconsrc: string | undefined; name: string }) {
   return(
     <a href={props.src} target="_blank">
       {props.icon ? <i className={'nf '+props.icon} /> : <></>} 
@@ -40,20 +40,20 @@ export default function Links() {
               <h2>Links:</h2>
 
               <Link 
-                name="Portfolio" src="https://vortygon.space" icon="nf-md-web" 
+                name="Portfolio" src="https://vortygon.space" icon="nf-md-web" iconsrc=''
               />
               
               <Link 
-                name="CopyCat" src="https://copycat.vortygon.space" 
+                name="CopyCat" src="https://copycat.vortygon.space" icon=""
                 iconsrc="https://copycat.vortygon.space/CopyCat_logo_white.svg" 
               />
 
               <Link 
-                name="Twitch" src="https://twitch.tv/vortygon" icon="nf-fa-twitch" 
+                name="Twitch" src="https://twitch.tv/vortygon" icon="nf-fa-twitch" iconsrc='' 
               />
 
               <Link 
-                name="X (Twitter)" src="https://x.com/vortygon" icon="nf-dev-twitter" 
+                name="X (Twitter)" src="https://x.com/vortygon" icon="nf-dev-twitter" iconsrc=''
               />
 
             </div>
