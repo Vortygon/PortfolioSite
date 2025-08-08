61d9f303f06d689d50044f00a640650a8a6f9fca
Vortygon <50510432+Vortygon@users.noreply.github.com>
2025-08-09 01:35:22 +0700

    Links tweaks

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -44,7 +44,8 @@ const links = [
 export default function Links() {
   return (
         <main className="antialiased flex min-h-screen bg-neutral-900 items-center justify-center md:p-24 p-6">
-          <Image src={"/avatar.png"} alt='' width={2048} height={2048} className='absolute object-cover blur-3xl opacity-30 h-full avatar'/>
+          <Image src={"/avatar.png"} alt='' width={2048} height={2048} className='absolute object-cover blur-3xl opacity-20 h-full avatar'/>
+
           <div id="root" 
           className='flex flex-col md:flex-row bg-neutral-800 backdrop-blur-3xl shadow-lg rounded-3xl overflow-hidden w-full z-10'>
 
@@ -66,7 +67,7 @@ export default function Links() {
                 <h2 className='font-semibold not-md:hidden'>Links:</h2>
 
                 {links.map((link,i) => 
-                  <a key={link.name+i} href={link.src} target="_blank" className='flex hover:-translate-y-1 duration-150 hover:shadow-lg hover:bg-neutral-600 rounded-2xl bg-neutral-700 p-3 md:mx-5 px-3 h-13 items-center gap-1'>
+                  <a key={link.name+i} href={link.src} target="_blank" className='flex hover:-translate-y-1 duration-150 hover:shadow-lg hover:bg-neutral-600 rounded-2xl bg-neutral-700 p-3 px-3 h-13 items-center gap-1'>
                     {link.icon ? <i className={'w-10 text-center nf '+link.icon} /> : <></>} 
                     {link.iconsrc ? <Image src={link.iconsrc} alt='' width={100} height={100} className='max-h-full w-10'/> : <></>} 
                     <div className='w-full text-center'>{link.name}</div>
